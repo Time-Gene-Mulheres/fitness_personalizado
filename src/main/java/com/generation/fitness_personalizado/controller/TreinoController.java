16949d5b0466200002b3e41dd68d884db8778ced
Jessica <jessica.rodrialves@outlook.com>
2025-03-13 14:58:25 -0300

    inclusão do metodo MVC classe Treino

--- src/main/java/com/generation/fitness_personalizado/controller/TreinoController.java
+++ src/main/java/com/generation/fitness_personalizado/controller/TreinoController.java
@@ -1,5 +1,94 @@
 package com.generation.fitness_personalizado.controller;
 
-public class TreinoController {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.fitness_personalizado.model.Treino;
+import com.generation.fitness_personalizado.repository.CategoriaRepository;
+import com.generation.fitness_personalizado.repository.TreinoRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+
+@RequestMapping("/treinos")
+
+@CrossOrigin (allowedHeaders = "*", origins = "*")
+
+public class TreinoController {
+	
+	@Autowired	
+	private TreinoRepository treinoRepository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	//Buscar todos treinos
+	@GetMapping	
+	public ResponseEntity<List<Treino>> getAll(){
+		return ResponseEntity.ok(treinoRepository.findAll());
+	}
+	
+	//Buscar treino por id
+	@GetMapping("/{id}")
+	public ResponseEntity<Treino> getById(@PathVariable Long id){
+		return treinoRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	//Buscar treinos por nome
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Treino>> getByNome(@PathVariable String nome){
+		return ResponseEntity.ok(treinoRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	//Cadastrar treino
+	@PostMapping
+	public ResponseEntity<Treino> post(@Valid @RequestBody Treino treino){
+		if(treinoRepository.existsById(treino.getCategoria().getId()))
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(treinoRepository.save(treino));
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+	}
+	
+	//Atualizar treino
+	@PutMapping
+	public ResponseEntity<Treino> put(@Valid @RequestBody Treino treino){
+		if(treinoRepository.existsById(treino.getId())) {
+			if(treinoRepository.existsById(treino.getCategoria().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(treinoRepository.save(treino));
+			
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+	
+	//Deletar treino	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Treino> treino = treinoRepository.findById(id);
+		
+		if(treino.isEmpty()) 
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		treinoRepository.deleteById(id);	
+	}
+	
 }
--- src/main/java/com/generation/fitness_personalizado/model/Treino.java
+++ src/main/java/com/generation/fitness_personalizado/model/Treino.java
@@ -1,15 +1,106 @@
 package com.generation.fitness_personalizado.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_treino")
+
 public class Treino {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank(message = "O atributo nome é obrigatorio")
 	private String nome;
 	
+	@NotBlank(message = "O atributo descrição é obrigatorio")
+	@Size(min = 5, message = " A descrição tem que ser maior que 5 caracteres")
 	private String descricao;
 	
+	@NotBlank(message = "O atributo duração é obrigatorio")
+	@Min(5)
 	private int duracao;
 	
+	//pensar em como restringir (Leve/Moderado/Intenso)
+	@NotBlank(message = "O atributo intensidade é obrigatorio")
+	@Size(min = 4, message = " A intensidade tem que ser maior que 4 caracteres")
+	@Pattern(regexp = "(?i)Leve|Moderado|Intenso", message = "A intensidade deve ser 'Leve', 'Moderado' ou 'Intenso'")
 	private String intensidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("usuario")
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("categoria")
+	private Categoria categoria;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(int duracao) {
+		this.duracao = duracao;
+	}
+
+	public String getIntensidade() {
+		return intensidade;
+	}
+
+	public void setIntensidade(String intensidade) {
+		this.intensidade = intensidade;
+	}
 	
 }
