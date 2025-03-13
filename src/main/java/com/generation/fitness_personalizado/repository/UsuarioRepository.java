6a6e4581907af8f1e65a3fda24a1275615ad359b
Sandra Mastrogiacomo <sandramastrogiacomo@hotmail.com>
2025-03-13 14:41:07 -0300

    implementação do  MVC  usuário

--- src/main/java/com/generation/fitness_personalizado/repository/UsuarioRepository.java
+++ src/main/java/com/generation/fitness_personalizado/repository/UsuarioRepository.java
@@ -1,5 +1,15 @@
 package com.generation.fitness_personalizado.repository;
 
-public interface UsuarioRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.fitness_personalizado.model.Usuario;
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	public List<Usuario> findAllByUsuarioContainingIgnoreCase(@Param("usuario") String usuario);
+	
 }
